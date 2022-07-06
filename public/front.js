546e7c9dab99795169f04c498f9d5512e4c268c3
ryanwmichaud <92121030+ryanwmichaud@users.noreply.github.com>
2022-07-06 11:22:43 -0500

    working as last sum and can clear w/out refresh

--- public/front.js
+++ public/front.js
@@ -2,17 +2,28 @@ const getBtn = document.getElementById("get")
 const postBtn = document.getElementById("post")
 const input = document.getElementById("input")
 const feed = document.getElementById('feed')
+const clearBtn = document.getElementById('clear')
 
 const baseURl = 'http://localhost:8383/'
 
 getBtn.addEventListener("click", getInfo)
 postBtn.addEventListener('click', postInfo)
+clearBtn.addEventListener('click', async (e)=>{
+    e.preventDefault()
+    input.value=0
+    await postInfo(e)
+    await postInfo(e)
+    while (feed.lastChild) {
+        feed.removeChild(feed.lastChild);
+    }
+      
+})
 
 async function getInfo(e){ //get info from server
     e.preventDefault()
     const res = await fetch(baseURl + 'doubled' , { method: 'GET' })//. option for fetch. get by default but well write it
     const data = await res.json() //get as json
-    input.value = data.info //info is the field we gave the res json
+    input.value = data.sum //info is the field we gave the res json
 }
 async function postInfo(e){ //send new value to server to update stuff
     e.preventDefault()
@@ -25,6 +36,6 @@ async function postInfo(e){ //send new value to server to update stuff
             parcel: input.value
         })
     })
-    feed.insertAdjacentHTML('afterbegin',`<p>${input.value}</p>`)
+    feed.insertAdjacentHTML('afterbegin',`<p class="data">${input.value}</p>`)
 }
 
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8383;
 
-var last = 0;
-var curr = 0;
-var sum = eval(last) + eval(curr);
+var last = 0
+var curr = 0
+var sum =  0
 
 app.use(express.static('public')) //default served at home route
 app.use(express.json())
@@ -22,12 +22,11 @@ app.get('/doubled', (req, res) => { //"send to" the server. really telling it wh
         })
 })
 
-app.post('/', (req,res)=>{ //receive the fetch post req. get the data and do stuff w it. here I jsut saved it globally. 
-    last = curr;
+app.post('/', (req,res)=>{ //receive the fetch post req w new data. get the data and do stuff w it. here I jsut saved it globally. 
+    last = curr
     const {parcel} = req.body
-    console.log(parcel);
-    curr = parcel;
-    sum = curr + last
+    curr = parcel
+    sum = parseInt(curr) + parseInt(last)
     if (!parcel){
         return res.status(400).send({status: 'failed'})
     }
